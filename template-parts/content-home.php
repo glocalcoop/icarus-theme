509c8339b990033da0c50fe615fc9b3fbe2f4db9
Pea Lutz <pea@glocal.coop>
2016-07-18 17:56:39 -0400

    Modified partial for homepage content.

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -9,12 +9,17 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'intro-content' ); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'intro' ); ?>>
+	<header class="entry-header intro-header">
+		<?php if( has_post_thumbnail() ) : ?>
+			<figure class="intro-image">
+			<?php the_post_thumbnail( 'thumbnail' ); ?>
+			</figure>
+		<?php endif; ?>
+		<?php the_title( '<h1 class="page-title intro-title screen-reader-text">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="entry-content intro-content">
 		<?php
 			the_content();
 
@@ -25,17 +30,4 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					esc_html__( 'Edit %s', 'icarus' ),
-					the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-		?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
