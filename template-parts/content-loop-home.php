33a995337d04773434fbf0e1c3823fcc436c159f
Pea Lutz <pea@glocal.coop>
2016-07-18 17:58:15 -0400

    Created partial for homepage post list. Removed post list from homepage template.

---
+++ template-parts/content-loop-home.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Icarus
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+
+		<?php if ( current_theme_supports( 'get_the_image' ) ) get_the_image(); ?>
+		
+		<?php
+			if ( is_single() ) {
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			} else {
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			}?>
+		<div class="entry-meta">
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			the_content( sprintf(
+				/* translators: %s: Name of current post. */
+				wp_kses( __( 'More %s <span class="meta-nav">&rarr;</span>', 'icarus' ), array( 'span' => array( 'class' => array() ) ) ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			) );
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'icarus' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+</article><!-- #post-## -->
