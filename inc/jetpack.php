f17af9af1a92b1f62b4743b38474cdac34d30962
Pea Lutz <pea@glocal.coop>
2016-07-18 23:07:51 -0400

    Removed support for infinite scroll.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -15,11 +15,11 @@
  */
 function icarus_jetpack_setup() {
 	// Add theme support for Infinite Scroll.
-	add_theme_support( 'infinite-scroll', array(
-		'container' => 'main',
-		'render'    => 'icarus_infinite_scroll_render',
-		'footer'    => 'page',
-	) );
+	// add_theme_support( 'infinite-scroll', array(
+	// 	'container' => 'main',
+	// 	'render'    => 'icarus_infinite_scroll_render',
+	// 	'footer'    => 'page',
+	// ) );
 
 	// Add theme support for Responsive Videos.
 	add_theme_support( 'jetpack-responsive-videos' );
