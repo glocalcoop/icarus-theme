8c8cc90ca550b8975e3c2f3b106e4853bbed922b
Pea Lutz <pea@glocal.coop>
2017-01-08 18:06:46 -0500

    Added default OpenGraph image filter.

--- inc/extras.php
+++ inc/extras.php
@@ -44,7 +44,7 @@ if( ! function_exists( 'icarus_count_widgets' ) ) {
   function icarus_count_widgets( $sidebar_id ) {
     // If loading from front page, consult $_wp_sidebars_widgets rather than options to see if wp_convert_widget_settings() has made manipulations in memory.
     $sidebars_widgets = wp_get_sidebars_widgets();
-    
+
     if ( isset( $sidebars_widgets ) ) :
 
       $widget_count = count( $sidebars_widgets[ $sidebar_id ] );
@@ -53,7 +53,7 @@ if( ! function_exists( 'icarus_count_widgets' ) ) {
         // Four widgets er row if there are exactly four or more than six
         $widget_classes .= ' per-row-4';
       elseif ( $widget_count >= 3 ) :
-        // Three widgets per row if there's three or more widgets 
+        // Three widgets per row if there's three or more widgets
         $widget_classes .= ' per-row-3';
       elseif ( 2 == $widget_count ) :
         // Otherwise show two widgets per row
@@ -67,3 +67,35 @@ if( ! function_exists( 'icarus_count_widgets' ) ) {
   }
 
 }
+
+/**
+ * Override Jetpack's Default OpenGraph Image
+ * If JetPack can't find an image to use, it uses a default image (https://s0.wp.com/i/blank.jpg)
+ *
+ * @uses jetpack_open_graph_image_default filter
+ * @uses the_custom_logo()
+ * @uses get_theme_mod()
+ * @uses wp_get_attachment_image_src()
+ *
+ * @since 1.0.3
+ *
+ * @return string $image[0] || https://s0.wp.com/i/blank.jpg
+ */
+function icarus_default_jetpack_open_graph_image() {
+
+ if ( function_exists( 'the_custom_logo' ) ) {
+
+	 $custom_logo_id = get_theme_mod( 'custom_logo' );
+
+	 if( !empty( $custom_logo_id ) ) {
+		 $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+
+		 return $image[0];
+	 }
+
+	 return esc_url( 'https://s0.wp.com/i/blank.jpg' );
+
+ }
+
+}
+add_filter( 'jetpack_open_graph_image_default', 'icarus_default_jetpack_open_graph_image' );
